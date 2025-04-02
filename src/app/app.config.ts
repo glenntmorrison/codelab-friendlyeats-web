ae3a70923a3c51021d88ab861c357b435f4830f0
Glenn Morrison <glenntmorrison@Mac.lan>
2025-04-01 21:43:38 -0600

    removed importProvidersFrom to fix Angular error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging()),
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-6f8ca","appId":"1:145078646792:web:59fb4c17106e9b199139bd","storageBucket":"friendlychat-6f8ca.firebasestorage.app","apiKey":"AIzaSyBns7mIBaB6nIG3HJVhbwtfKM4SJFLaEXY","authDomain":"friendlychat-6f8ca.firebaseapp.com","messagingSenderId":"145078646792"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
